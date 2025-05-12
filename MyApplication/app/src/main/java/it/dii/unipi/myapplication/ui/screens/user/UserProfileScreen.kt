5697d7652583600bc124c3242d17d6abbc253d2a
loremancio <l.mancinelli1@studenti.unipi.it>
2025-05-12 16:36:42 +0200

    update of the loading of the achievements of the user

--- MyApplication/app/src/main/java/it/dii/unipi/myapplication/ui/screens/user/UserProfileScreen.kt
+++ MyApplication/app/src/main/java/it/dii/unipi/myapplication/ui/screens/user/UserProfileScreen.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -20,8 +22,11 @@ class UserProfileScreen : Fragment() {
 
     companion object {
         private const val TAG = "UserProfileScreen"
-        private const val BASE_URL = "http://192.168.64.250:5000/user_summary" // <-- Sostituisci con l'indirizzo corretto
+        private const val BASE_URL = "http://192.168.64.250:5000/user_summary" // Sostituisci con il tuo IP reale
     }
+
+    data class Achievement(val title: String, val description: String)
+    data class UserAchievements(val username: String, val achievements: List<Achievement>)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,7 +38,6 @@ class UserProfileScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "UserProfileScreen: onViewCreated")
         fetchUserSummary(view)
     }
 
@@ -45,25 +49,51 @@ class UserProfileScreen : Fragment() {
 
         lifecycleScope.launch {
             try {
-                val (username, recordingsCount) = withContext(Dispatchers.IO) {
+                val userAchievements = withContext(Dispatchers.IO) {
                     val response = client.newCall(request).execute()
                     if (!response.isSuccessful) throw Exception("Errore HTTP: ${response.code}")
-                    val body = response.body?.string() ?: throw Exception("Corpo della risposta vuoto")
+                    val body = response.body?.string() ?: throw Exception("Corpo vuoto")
+
                     val json = JSONObject(body)
                     val username = json.getString("username")
-                    val count = json.getInt("recordings_count")
-                    Log.d(TAG, "Username: $username, Recordings Count: $count")
-                    username to count
+                    val achievementsJson = json.getJSONArray("achievements")
+
+                    val achievements = mutableListOf<Achievement>()
+                    for (i in 0 until achievementsJson.length()) {
+                        val a = achievementsJson.getJSONObject(i)
+                        achievements.add(Achievement(a.getString("title"), a.getString("description")))
+                    }
+
+                    UserAchievements(username, achievements)
                 }
 
-                // Solo aggiornamento UI nel Main Thread
-                view.findViewById<TextView>(R.id.tvUserName).text = username
-                view.findViewById<TextView>(R.id.countTextViewRegistered).text = recordingsCount.toString()
+                withContext(Dispatchers.Main) {
+                    view.findViewById<TextView>(R.id.tvUserName).text = userAchievements.username
 
+                    val badgeData = listOf(
+                        Triple("City Explorer", R.id.iconBadgeCities to R.id.countTextViewCities, R.id.blockBadgeCities),
+                        Triple("World Traveler", R.id.iconBadgeCountries to R.id.countTextViewCountries, R.id.blockBadgeCountries),
+                        Triple("Measurement Master", R.id.iconBadgeRegistrations to R.id.countTextViewRegistered, R.id.blockBadgeRegistrations)
+                    )
+
+                    for ((title, ids, layoutId) in badgeData) {
+                        val (iconId, textId) = ids
+                        val match = userAchievements.achievements.firstOrNull { it.title == title }
+
+                        val blockLayout = view.findViewById<LinearLayout>(layoutId)
+
+                        if (match != null) {
+                            view.findViewById<ImageView>(iconId).setImageResource(R.drawable.ic_badge_col)
+                            view.findViewById<TextView>(textId).text = match.description
+                            blockLayout.visibility = View.VISIBLE
+                        } else {
+                            blockLayout.visibility = View.GONE
+                        }
+                    }
+                }
             } catch (e: Exception) {
                 Log.e(TAG, "Errore nella richiesta OkHttp", e)
             }
         }
     }
-
 }
